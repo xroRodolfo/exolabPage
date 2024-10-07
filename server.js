a7a0d693f09e31ec73cb842c8c4b1fa16f4cae54
xroRodolfo <alexramosr2002@gmail.com>
2024-10-06 20:37:14 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Configuración del puerto
+const PORT = process.env.PORT || 3000;
+
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para servir el archivo index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
